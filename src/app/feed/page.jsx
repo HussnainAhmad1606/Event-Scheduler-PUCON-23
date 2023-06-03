be9f7b0571fe6e0a2b886211697501874781612f
Muaz Khalid <itzmajo786@gmail.com>
2023-06-03 12:32:48 +0500

    Add cards in Feed

--- src/app/feed/page.jsx
+++ src/app/feed/page.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import Card from "../../components/Card"
 function page() {
   return (
+    <div className='grid gap-10 ml-7 grid-cols-3 '>
     <Card/>
+    
+    
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    
+    </div>
   )
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,14 @@ import React from 'react'
 function Card() {
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <figure><img src="https://images.unsplash.com/photo-1505373877841-8d25f7d46678?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=812&q=80" alt="Event Pic" /></figure>
   <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 className="card-title">PUCON'23</h2>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum rem quas porro dolores similique inventore sunt delectus harum natus quod beatae accusamus quidem, vitae voluptatem, repellat eaque impedit minus sint optio deleniti!
+    <p> 
+    </p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <button className="btn btn-primary">Event Details</button>
     </div>
   </div>
 </div>
